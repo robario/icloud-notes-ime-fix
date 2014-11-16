1296e4dd354b5e9666ad9ed0b20b74e3dfb80a45
robario <webmaster@robario.com>
2014-11-16 16:54:18 +0900

    Fix to work when it is launched from a top page

--- icloud-notes-ime-fix.user.js
+++ icloud-notes-ime-fix.user.js
@@ -1,35 +1,28 @@
 // ==UserScript==
 // @name icloud-notes-ime-fix.user.js
 // @namespace http://www.robario.com/
-// @version 0.1.2
+// @version 0.1.3
 // @author robario <webmaster@robario.com>
 // @description Fix the IME problem of iCloud notes.
 // @updateURL https://raw.githubusercontent.com/robario/icloud-notes-ime-fix/master/icloud-notes-ime-fix.user.js
-// @match https://www.icloud.com/*
+// @match https://www.icloud.com/
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @grant unsafeWindow
 // ==/UserScript==
-jQuery(function($) {
-    if (location.hash !== '#notes') {
+setTimeout(function() {
+    var context;
+    if (location.hash !== '#notes' || !(context = jQuery('iframe', jQuery('iframe').get(0).contentDocument).get(0))) {
+        setTimeout(arguments.callee, 1000);
         return;
     }
-
-    setTimeout(function() {
-        var context = $('iframe', $('iframe').get(0).contentDocument).get(0);
-        if (!context) {
-            setTimeout(arguments.callee, 1000);
-            return;
-        }
-
-        var lastKeyDown = 0;
-        $('#tinymce', context.contentDocument)
-          .on('keydown', function(event) {
-              lastKeyDown = event.which;
-          })
-          .on('keyup', function(event) {
-              if (lastKeyDown == 229 && event.which == 32) {
-                  event.stopPropagation();
-              }
-          });
-    }, 1000);
-});
+    var lastKeyDown = 0;
+    jQuery('#tinymce', context.contentDocument)
+        .on('keydown', function(event) {
+            lastKeyDown = event.which;
+        })
+        .on('keyup', function(event) {
+            if (lastKeyDown == 229 && event.which == 32) {
+                event.stopPropagation();
+            }
+        });
+}, 0);
